44d42490413d7538f92d45bc3714b38523a7e833
Thomas Tashjian <thomas.tashjian@ni.com>
2024-06-25 20:44:17 +0400

    started HomeWork8

---
+++ src/homework/HomeWork8/Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualBasic;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Buffers.Text;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare and Assign Enum Constants:
+            // Declare an enum representing different weather conditions(e.g., Sunny, Cloudy, Rainy).
+            // Assign one of its constants to a variable and print a message based on the weather.
+
+            var weatherCondition = WeatherConditions.Sunny;
+            // string weatherName = (string)WeatherConditions.Sunny; //error
+            string weatherConditionsEnumToString = WeatherConditions.Sunny.ToString();
+
+            Console.WriteLine(weatherCondition);
+            Console.WriteLine(weatherConditionsEnumToString);
+        }
+
+        enum WeatherConditions
+        {
+            Sunny,
+            Cloudy,
+            Rainy
+        }
+    }
+}
