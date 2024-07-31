6fbf0c56b4de70a55256ae7510f7d575ea83a9af
Thomas Tashjian <thomas.tashjian@ni.com>
2024-07-31 22:31:19 +0400

    adding practice code from 25-06-2024

---
+++ src/practice/practice25-07/methods_most_repeated/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace methods_most_repeated
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 1, 1, 1, 1, 1, 2, 2, 2, 2 };
+
+
+        }
+
+        static int GetMostRepeatedValue(int[] ints)
+        {
+            int result = 0;
+
+            int[] counts = new int[ints.Length];
+
+            for(int i = 0; i < ints.Length; ++i)
+            {
+
+            }
+
+            return result;
+        }
+    }
+}
