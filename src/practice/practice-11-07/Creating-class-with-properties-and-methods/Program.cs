7152e2682333b4e1b6e18d3de96a4c1674665ff8
Thomas Tashjian <thomas.tashjian@ni.com>
2024-07-31 22:31:34 +0400

    adding practice code from 11-07-2024

---
+++ src/practice/practice-11-07/Creating-class-with-properties-and-methods/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Creating_class_with_properties_and_methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ src/practice/practice-11-07/Creating-class-with-properties-and-methods/University.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creating_class_with_properties_and_methods
+{
+    class University
+    {
+        public String Name { get; set; }
+        public DateTime FoundedAt { get; set; }
+        private Student[] Students { get; set; }
+
+        private int lastIndex;
+
+        public University(string name)
+        {
+            Students = new Student[1000];
+            FoundedAt = DateTime.Now;
+            Name = name;
+            lastIndex = 0;
+        }
+
+        public void GetAdmitted(String studentName, int id)
+        {
+            if(lastIndex == (Students.Length - 1))
+            {
+                Array.Resize<Student>(ref Students, Students.Length*2);
+            }
+        }
+
+        public void Remove(String studentName)
+        {
+            
+        }
+
+        public void Graduate(String studentName)
+        {
+
+        }
+    }
+}
