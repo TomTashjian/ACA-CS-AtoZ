95f23d4bbc711514d6810bb2158bbbc21c9534e8
Thomas Tashjian <thomas.tashjian@ni.com>
2024-07-31 22:23:23 +0400

    Started HomeWork13. Completed Task 1 out of 5

---
+++ src/homework/HomeWork13/Task1 - Basic Interface Implementation/Circle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Circle : IShape
+    {
+        double pi = 3.14;
+        public double Radius { get; set; }
+
+        public double CalculateArea()
+        {
+            return pi * Radius * Radius;
+        }
+    }
+}
---
+++ src/homework/HomeWork13/Task1 - Basic Interface Implementation/Rectangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Rectangle : IShape
+    {
+        public double Width { get; set; }
+        public double Length { get; set; }
+
+        public double CalculateArea()
+        {
+            return Width * Length;
+        }
+    }
+}
