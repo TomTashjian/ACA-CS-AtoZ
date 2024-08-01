252d66e62542ec6b55145b035d5e301864b456e2
Thomas Tashjian <thomas.tashjian@ni.com>
2024-08-01 19:04:33 +0400

    adding Task2.

---
+++ src/homework/HomeWork13/Task2 - Explicit Interface Implementation/Program.cs
@@ -0,0 +1,21 @@
+﻿// Explicit Interface Implementation:
+// Define an interface IAnimal with methods Eat() and Sleep().
+// Create a class Dog that implements IAnimal. Implement the methods explicitly (using the IAnimal. prefix).
+// Create a Dog object and call the Eat() and Sleep() methods.
+
+namespace Task2___Explicit_Interface_Implementation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dog dog = new Dog();
+            // dog.Eat(); // error
+            // dog.Sleep(); //error
+            
+            IAnimal animal = new Dog();
+            animal.Eat();
+            animal.Sleep();
+        }
+    }
+}
