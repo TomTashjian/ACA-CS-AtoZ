f3a1a0059dbad6078a1c9cbc30dbc131581f77cb
Thomas Tashjian <thomas.tashjian@ni.com>
2024-08-02 16:07:43 +0400

    Task5

---
+++ src/homework/HomeWork13/Task5 - Challenge - Dependency Injection/FileLogger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    internal class FileLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            File.WriteAllText("log.txt", message);
+        }
+
+        public FileLogger()
+        {
+            
+        }
+    }
+}
