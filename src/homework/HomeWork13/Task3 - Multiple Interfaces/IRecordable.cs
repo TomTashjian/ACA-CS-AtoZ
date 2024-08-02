440cec168539502bd7ac36ff18db4e59ee261b58
Thomas Tashjian <thomas.tashjian@ni.com>
2024-08-02 13:43:35 +0400

    Task3

---
+++ src/homework/HomeWork13/Task3 - Multiple Interfaces/IRecordable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3___Multiple_Interfaces
+{
+    public interface IRecordable
+    {
+        public void Record();
+        public void StopRecording();
+    }
+}
---
+++ src/homework/HomeWork13/Task3 - Multiple Interfaces/Program.cs
@@ -0,0 +1,20 @@
+﻿// Multiple Interfaces:
+// Create an interface IPlayable with methods Play() and Pause().
+// Define another interface IRecordable with methods Record() and StopRecording().
+// Create a class MediaPlayer that implements both interfaces.
+// Create an instance of MediaPlayer and test its play, pause, record, and stop recording functionality.
+
+namespace Task3___Multiple_Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MediaPlayer mediaPlayer = new MediaPlayer();
+            mediaPlayer.Play();
+            mediaPlayer.Pause();
+            mediaPlayer.Record();
+            mediaPlayer.StopRecording();
+        }
+    }
+}
