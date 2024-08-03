9d80b1bb2b3b28156b1dce77b84c692e78d2d010
Thomas Tashjian <thomas.tashjian@ni.com>
2024-08-03 04:14:43 +0400

    HomeWork14, Tas2.

---
+++ src/homework/HomeWork14/Task2 - Library Bookshelf/Librarian.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Librarian : IEnumerator
+    {
+        private Book[] _bookshelf { get; set; }
+
+        public object? Current { get; private set; }
+
+        private int _currentIndex = -1;
+
+        public Librarian(Book[] bookshelf)
+        {
+            _bookshelf = bookshelf;
+        }
+
+        public bool MoveNext()
+        {
+            if(_currentIndex >= _bookshelf.Length)
+            {
+                return false;
+            }
+            else
+            {
+                _currentIndex++;
+                if (_bookshelf[_currentIndex] == null)
+                {
+                    return false;
+                }
+                Current = _bookshelf[_currentIndex];
+                return true;
+            }
+        }
+
+        public void Reset()
+        {
+            _currentIndex = -1;
+        }
+    }
+}
