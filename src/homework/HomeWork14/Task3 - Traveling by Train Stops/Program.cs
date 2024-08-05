09700d26cd7e733da75879a483aa84f3b0c826a8
Thomas Tashjian <thomas.tashjian@ni.com>
2024-08-05 21:38:12 +0400

    Task3

---
+++ src/homework/HomeWork14/Task3 - Traveling by Train Stops/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Traveling by Train Stops:
+Consider a train route with multiple stops (attractions).
+IEnumerable is your train ticket, allowing you to travel from one stop to the next.
+At each station (iteration), you explore the surroundings (access details) without knowing the entire route upfront.
+ */
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Train_Ticket.TicketType ticketType = Train_Ticket.TicketType.Platinum;
+
+            Train_Ticket ticket = new Train_Ticket(ticketType);
+
+            Console.WriteLine($"Here are the stations included in {ticketType} type ticket:");
+            foreach (Train_Station station in ticket)
+            {
+                Console.WriteLine(station.Name);
+            }
+        }
+    }
+}
---
+++ src/homework/HomeWork14/Task3 - Traveling by Train Stops/Train Route.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class Train_Route : IEnumerator
+    {
+        private List<Train_Station> _trainStations = new List<Train_Station>();
+        private int _currentIndex = -1;
+        public Train_Route(List<Train_Station> trainStations) 
+        {
+            _trainStations = trainStations;
+        }
+        public object? Current { 
+            get 
+            {
+                return _trainStations[_currentIndex];
+            } 
+        }
+
+        public bool MoveNext()
+        {
+            if(_currentIndex >= (_trainStations.Count - 1))
+            {
+                return false;
+            }
+            else
+            {
+                _currentIndex++;
+                return true;
+            }
+        }
+
+        public void Reset()
+        {
+            _currentIndex = -1;
+        }
+    }
+}
