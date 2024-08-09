15c139599328038b00d3fbcb6332c546afd8edc0
Thomas Tashjian <thomas.tashjian@ni.com>
2024-08-09 18:16:58 +0400

    added Task 1.

---
+++ src/homework/HomeWork15/Task1 - Custom Range Generator/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Custom Range Generator:
+Create a custom range generator using yield return.
+The generator should take a start value and an end value and produce all integers in that range.
+Example usage: foreach (var number in RangeGenerator(5, 10)) { Console.Write(number + " "); } should output 5 6 7 8 9 10
+ */
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(var number in new RangeGenerator(5, 10))
+            {
+                Console.Write(number + " ");
+            }
+        }
+    }
+}
---
+++ src/homework/HomeWork15/Task1 - Custom Range Generator/RangeGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class RangeGenerator : IEnumerable
+    {
+        private int _start { get; set; }
+        private int _end { get; set; }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = _start; i <= _end; i++)
+            {
+                yield return i;
+            }
+        }
+
+        public RangeGenerator(int start, int end)
+        {
+            _start = start;
+            _end = end;
+        }
+    }
+}
