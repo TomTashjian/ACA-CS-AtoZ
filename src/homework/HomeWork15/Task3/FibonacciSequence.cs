39a8c6e4c2c4e3bfa1963cafaef4bb950c9c3855
Thomas Tashjian <thomas.tashjian@ni.com>
2024-08-15 01:21:25 +0400

    added Task3 - Fibonacci Sequence

---
+++ src/homework/HomeWork15/Task3/FibonacciSequence.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class FibonacciSequence : IEnumerable
+    {
+        private int _limit { get; set; }
+
+        public FibonacciSequence(int limit)
+        {
+            _limit = limit;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            double a = 1;
+            double b = 0;
+            double seqItem = 0;
+            for(int i = 0; i < _limit; i++)
+            {
+                seqItem = a + b;
+                a = b;
+                b = seqItem;
+                yield return seqItem;
+            }
+        }
+    }
+}
---
+++ src/homework/HomeWork15/Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Generate the Fibonacci sequence using yield return.
+Create an iterator that produces Fibonacci numbers up to a specified limit.
+Example: foreach (var fib in FibonacciSequence(100)) { Console.Write(fib + " "); }.
+*/
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var fib in new FibonacciSequence(100))
+            {
+                System.Console.Write(fib + " ");
+            }
+        }
+    }
+}
