8585e4e324e161395bb601344a8e2bc9aae890b1
Thomas Tashjian <thomas.tashjian@ni.com>
2024-09-18 13:32:00 +0400

    "FileIO-1" practice code project  - written Person objects list into a CSV file using the static File class

---
+++ src/practice/practice-fileio/PracticeFileIO/FileIO-1/PersonFileUtility.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIO_1
+{
+    internal static class PersonFileUtility
+    {
+        public static string GetPersonString(Person person, bool addLabel = false)
+        {
+            string result = "";
+            if (addLabel)
+            {
+                result += "First Name, Last Name, Birth Date\n";
+            }
+            result += $"{person.FirstName}, {person.LastName}, {person.BirthDate}";
+            return result;
+        }
+    }
+}
---
+++ src/practice/practice-fileio/PracticeFileIO/FileIO-1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Transactions;
+
+namespace FileIO_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> PersonList = new List<Person>()
+            {
+                new Person() { FirstName = "John", LastName = "Doe", BirthDate = DateOnly.Parse("1/1/1980")},
+                new Person() { FirstName = "Tom", LastName = "Smith", BirthDate = DateOnly.Parse("2/2/1990")}
+            };
+
+            List<string> PersonStrings = new List<string>();
+            foreach (Person person in PersonList)
+            {
+                PersonStrings.Add(PersonFileUtility.GetPersonString(person, PersonList.IndexOf(person) == 0));
+            }
+
+            Console.WriteLine("writing to CSV file..");
+            File.WriteAllLines("Persons.csv", PersonStrings);
+            Console.WriteLine("done");
+
+            Console.WriteLine("reading from CSV file..");
+            string[] FileLines= File.ReadAllLines("Persons.csv");
+            for(int i = 0; i < FileLines.Length; ++i)
+            {
+                Console.WriteLine(FileLines[i]);
+            }
+        }
+    }
+}
