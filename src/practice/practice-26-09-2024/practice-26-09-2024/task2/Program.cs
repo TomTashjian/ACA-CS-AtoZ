0b815f7bd886e273e781355788ad47654a5aaedf
Thomas Tashjian <thomas.tashjian@ni.com>
2024-09-26 21:43:19 +0400

    tasks with predicates, IEnumerable, extensions

---
+++ src/practice/practice-26-09-2024/practice-26-09-2024/task2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<int> ints = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var filteredInts = Filter(ints, i => i % 4 == 0);
+
+            foreach (int i in filteredInts)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static IEnumerable<int> Filter(IEnumerable<int> ints, Predicate<int> predicate)
+        {
+            foreach (int i in ints)
+            {
+                if (predicate(i))
+                {
+                    yield return i;
+                }
+            }
+        }
+    }
+}
