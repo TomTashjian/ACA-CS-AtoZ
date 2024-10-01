cced270b17c68b9a0558dee9aaf2005d444fdbf4
Thomas Tashjian <thomas.tashjian@ni.com>
2024-10-01 21:05:46 +0400

    task3 incomplete

---
+++ src/practice/practice-28-09-2024/task3/Data.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    public static class MyData
+    {
+        public static List<Pet> pets = new List<Pet>() 
+        { 
+            new Pet() { Id = 0, Name = "Max"},
+            new Pet() { Id = 1, Name = "Hancho"},
+            new Pet() { Id = 2, Name = "Caesar"},
+        };
+
+        public static List<Person> persons = new List<Person>()
+        {
+            new Person() { age = 20, name = "Felix"},
+            new Person() { age = 21, name = "Harry"},
+            new Person() { age = 22, name = "Caesar"},
+        };
+    }
+}
