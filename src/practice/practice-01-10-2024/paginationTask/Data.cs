35a8e5d812ab09245fc52700aabff16e5d91175d
Thomas Tashjian <thomas.tashjian@ni.com>
2024-10-01 21:42:23 +0400

    pagination example task

---
+++ src/practice/practice-01-10-2024/paginationTask/Data.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paginationTask
+{
+    public class Data
+    {
+        public static IEnumerable<Pet> Pets = Enumerable.Range(1, 1000000)
+            .Select(i => new Pet(i, $"Pet{i * -1}", PetType.Dog, i % 100)).ToArray();
+    }
+}
---
+++ src/practice/practice-01-10-2024/paginationTask/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace paginationTask
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            PaginationRequest request = new PaginationRequest
+            {
+                PageNumber = 1,
+                PageSize = 10,
+                OrderingField = "Id",
+                Ascending = true
+            };
+
+            PaginationResult<Pet> result = new PaginationResult<Pet>
+            {
+                Items = Data.Pets.OrderBy(p => p.Id)
+                    .Skip((request.PageNumber - 1) * request.PageSize)
+                    .Take(request.PageSize)
+                    .ToArray(),
+                TotalItems = Data.Pets.Count()
+            };
+
+            Console.ReadKey();
+        }
+    }
+
+    public class PaginationRequest
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public string? OrderingField { get; set; }
+        public bool Ascending { get; set; }
+
+    }
+
+    public class PaginationResult<T>
+    {
+        public int TotalItems { get; set; }
+        public T[] Items { get; set; }
+    }
+}
